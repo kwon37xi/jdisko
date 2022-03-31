5e532081eaa5125e744d896fd63c0745d5e05455
KwonNam Son <kwon37xi@gmail.com>
2022-03-31 23:22:26 +0900

    ListCommand : narrow packages

--- app/src/main/java/com/github/kwon37xi/jdisko/commands/ListCommand.java
+++ app/src/main/java/com/github/kwon37xi/jdisko/commands/ListCommand.java
@@ -1,12 +1,16 @@
 package com.github.kwon37xi.jdisko.commands;
 
+import eu.hansolo.jdktools.Architecture;
+import eu.hansolo.jdktools.Latest;
+import eu.hansolo.jdktools.OperatingSystem;
+import eu.hansolo.jdktools.PackageType;
 import io.foojay.api.discoclient.pkg.Distribution;
 import io.foojay.api.discoclient.pkg.Pkg;
 import picocli.CommandLine.Command;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 
 @Command(
         name = "list",
@@ -17,11 +21,18 @@ public class ListCommand extends BaseCommand implements Runnable {
     public void run() {
         final Map<String, Distribution> distros = discoClient().getDistros();
         final Distribution temurin = distros.get("temurin");
-        final List<Pkg> pkgs = discoClient().getPkgs(List.of(temurin), null, null, null, null, null, null, null, null, null, null, null,
+        final String osName = System.getProperty("os.name");
+        final OperatingSystem operatingSystem = OperatingSystem.fromText(osName);
+        final String osArch = System.getProperty("os.arch");
+        final Architecture architecture = Architecture.fromText(osArch);
+
+        System.out.printf("OS %s(%s), arch : %s(%s) ", operatingSystem, osName, architecture, osArch);
+        final List<Pkg> pkgs = discoClient().getPkgs(List.of(temurin), null, Latest.AVAILABLE, operatingSystem, operatingSystem.getLibCType(), architecture, null, null, PackageType.JDK, null, null, null,
                 null, null, null, null);
+        pkgs.sort(Comparator.comparing(pkg -> pkg.getJavaVersion()));
         pkgs.forEach(pkg -> {
             final Distribution distribution = pkg.getDistribution();
-            System.out.printf("%s %s%n", distribution.getName(), pkg.getJavaVersion());
+            System.out.printf("%s %s %s%n", distribution.getName(), pkg.getJavaVersion(), pkg);
         });
     }
 }
