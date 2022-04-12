d244fb0e43094f46e6c81b231f8a8a2f600f5da6
KwonNam Son <kwon37xi@gmail.com>
2022-04-12 16:16:50 +0900

    DistributionsCommand: --names-only option

--- app/src/main/java/com/github/kwon37xi/jdisko/commands/DistributionsCommand.java
+++ app/src/main/java/com/github/kwon37xi/jdisko/commands/DistributionsCommand.java
@@ -3,6 +3,7 @@ package com.github.kwon37xi.jdisko.commands;
 import eu.hansolo.jdktools.OperatingSystem;
 import io.foojay.api.discoclient.pkg.Distribution;
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
 
 import java.util.List;
 
@@ -13,6 +14,13 @@ import java.util.List;
 )
 public class DistributionsCommand extends BaseCommand implements Runnable {
 
+    @Option(
+            names = {"-n", "--names-only"},
+            description = "only print distribution names",
+            defaultValue = "false"
+    )
+    private boolean namesOnly = false;
+
     @Override
     protected OperatingSystem operatingSystem() {
         return super.operatingSystem();
@@ -21,8 +29,11 @@ public class DistributionsCommand extends BaseCommand implements Runnable {
     @Override
     public void run() {
         final List<Distribution> distributions = discoClient().getDistributions();
-        distributions.forEach(distribution -> {
-            System.out.printf("%20s\t%s\t%s%n", distribution.getName().toLowerCase(), distribution.getUiString(), distribution.isMaintained());
-        });
+
+            if (namesOnly) {
+                distributions.forEach(distribution -> System.out.printf("%s%n", distribution.getName().toLowerCase()));
+            } else {
+                distributions.forEach(distribution -> System.out.printf("%20s\t%s\t%s%n", distribution.getName().toLowerCase(), distribution.getUiString(), distribution.isMaintained()));
+            }
     }
 }
